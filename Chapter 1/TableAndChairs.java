550d6bd974a112a1e9c71427ff9e136a99cbb261
jhonrovic <jhonrovic7@gmail.com>
2024-08-14 22:35:35 +0800

    Add files via upload

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TableAndChairs{
+
+    public static void main(String[] args) {
+      
+        printTableAndChairPattern();
+    }
+
+    public static void printTableAndChairPattern() {
+        
+        System.out.println("T                              T");
+        
+        System.out.println("T                              T");
+        
+        System.out.println("T          TTTTTTTTTT          T");
+        
+        System.out.println("TTTTT      T        T      TTTTT");
+        
+        System.out.println("T   T      T        T      T   T");
+        
+        System.out.println("T   T      T        T      T   T");
+    }
+    
+    }
