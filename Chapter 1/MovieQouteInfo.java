4580c7a0b9ed3ecef73e7f67959f047287cbce82
jhonrovic <jhonrovic7@gmail.com>
2024-08-21 19:50:18 +0800

    Add files via upload

---
+++ Chapter 1/MovieQouteInfo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MovieQouteInfo {
+    public static void main(String[] args) {
+        System.out.println("\"Working hard is important. But there is something that matters even more, believing in yourself\"");
+        System.out.println("Movie:Harry Potter and the Sorcerer's Stone");
+        System.out.println("Character: Harry potter");
+        System.out.println("Year: 2001");
+    }
+}
