4f76ca978b08f372cee376a5f70155fd41fb8eb4
jhonrovic <jhonrovic7@gmail.com>
2024-09-02 00:10:31 +0800

    Add files via upload

---
+++ Chapter 1/warmup_sleepIn.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class warmup_sleepIn {
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(sleepIn(false, false)); 
+        System.out.println(sleepIn(true, false));  
+        System.out.println(sleepIn(false, true));  
+    }
+}
