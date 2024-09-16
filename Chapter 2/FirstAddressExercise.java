88e8927759eef554d44fa4f1f611e69fdb3867f7
jhonrovic <jhonrovic7@gmail.com>
2024-09-16 21:30:17 +0800

    Add files via upload

---
+++ Chapter 2/FirstAddressExercise.java
@@ -0,0 +1,18 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        
+        System.out.println("First Java application");
+        
+        System.out.println("Headquarters Address of Apple:");
+        displayAddress();
+    }
+    
+       public static void displayAddress() {
+      
+        String address = "Apple Inc.\nOne Apple Park Way\nCupertino, CA 95014\nUnited States";
+        
+       
+        System.out.println(address);
+    }
+}
