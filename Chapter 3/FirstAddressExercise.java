d64b9e7b04b9d8fb8c2a2dfe5149467e634e508d
jhonrovic <jhonrovic7@gmail.com>
2024-09-18 08:35:49 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,18 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        
+        System.out.println("First Java application");
+        
+        System.out.println("Headquarters Address of Huawei:");
+        displayAddress();
+    }
+    
+       public static void displayAddress() {
+      
+        String address = "Huawei industrial Base.\nBantian,Longgang District\nShenzhen, Guangdong 58129\nChina";
+        
+       
+        System.out.println(address);
+    }
+}
