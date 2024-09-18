363c9fb35ce82996848a00d1e03d997af5c6dbd8
jhonrovic <jhonrovic7@gmail.com>
2024-09-18 08:41:47 +0800

    Delete Chapter 2/SwingAlert.java

--- Chapter 2/SwingAlert.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class SwingAlert {
-    public static void main(String[] args) {
-      
-        JOptionPane.showMessageDialog(null, "Hello World!!", "Message", JOptionPane.INFORMATION_MESSAGE);
-    }
-}
-
